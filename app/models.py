2cfb5f2e08b301831dfe8a57c923685a8bad4573
kimnganvvv <ntkn26062005@gmail.com>
2026-03-17 18:53:55 +0700

    update trangchu

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, String, Integer, Time, Boolean, Text, ForeignKey, DateTime, Date, Enum, text,func
+from sqlalchemy import Column, String, Integer, Boolean, Text, ForeignKey, DateTime, Date, Enum
 from enum import Enum as CustomEnum
-
 from app import db, app
 
+class Base(db.Model):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, autoincrement=True)
 
-class Customer(db.Model):
-    id = Column(db.Integer, primary_key=True, autoincrement=True)
+class Customer(Base):
     full_name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
     phone_number = Column(String(100), nullable=False)
@@ -13,35 +14,32 @@ class Customer(db.Model):
     password = Column(String(100), nullable=False)
     # birthday = Column(Date, nullable=False)
 
-class RoomType(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class RoomType(Base):
     name = Column(String(100), nullable=False)
     capacity = Column(Integer, nullable=False)
 
-class Room(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class Room(Base):
     type = Column(Integer, ForeignKey(RoomType.id), nullable=False)
 
-class MovieType(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class MovieType(Base):
     name = Column(String(100), nullable=False)
 
-class Movie(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class Movie(Base):
     title = Column(String(100), nullable=False)
     description = Column(Text, nullable=False)
     age_limit = Column(Integer, nullable=False)
     duration = Column(Integer, nullable=False)
-    type = Column(Integer, ForeignKey(MovieType.id), nullable=False)
 
-class MovieScreening(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class MovieTypeDetail(Base):
+    type = Column(Integer, ForeignKey(MovieType.id), nullable=False)
+    movie = Column(Integer, ForeignKey(Movie.id), nullable=False)
+
+class MovieScreening(Base):
     start_time = Column(DateTime, nullable=False)
     room = Column(Integer, ForeignKey(Room.id), nullable=False)
     movie = Column(Integer, ForeignKey(Movie.id), nullable=False)
 
-class Seat(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class Seat(Base):
     location = Column(String(100), nullable=False)
     room = Column(Integer, ForeignKey(Room.id), nullable=False)
 
@@ -52,15 +50,13 @@ class TicketStatus(CustomEnum):
     CANCELLED = 3,
     USED = 4
 
-class Ticket(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class Ticket(Base):
     price = Column(Integer, nullable=False)
     status = Column(Enum(TicketStatus), nullable=False, default=TicketStatus.AVAILABLE)
     customer = Column(Integer, ForeignKey(Customer.id), nullable=False)
     seat = Column(Integer, ForeignKey(Seat.id), nullable=False)
 
-class TicketBill(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class TicketBill(Base):
     pay_time = Column(DateTime, nullable=False)
     is_paid = Column(Boolean, nullable=False)
     ticket = Column(Integer, ForeignKey(Ticket.id), nullable=False)
