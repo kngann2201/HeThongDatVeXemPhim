f11225a3173da24977476cfb8c85477a381f6c13
kngann2201 <kngann2201@gmail.com>
2026-05-08 22:02:49 +0700

    hidden repay when success,fix next_page url, sel login & register

---
+++ app/test/selenium/pages/BasePage.py
@@ -0,0 +1,31 @@
+import time
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def find(self, by, value):
+        return self.driver.find_element(by, value)
+
+    def finds(self, by, value):
+        return self.driver.find_elements(by, value)
+
+    def click(self, by, value):
+        e = self.find(by, value)
+        self.driver.execute_script("arguments[0].scrollIntoView();", e)
+        time.sleep(1)
+        e.click()
+
+    def typing(self, by, value, text):
+        e = self.find(by, value)
+        e.send_keys(text)
+
+    def upload_file(self, by, value, file_path):
+        self.find(by, value).send_keys(file_path)
+
+    def set_date(self, by, value, date_value):
+        self.find(by, value).send_keys(date_value)
