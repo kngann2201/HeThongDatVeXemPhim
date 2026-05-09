5d479480a9b3ee93abf2e4e35e4c7b35252a9887
kngann2201 <kngann2201@gmail.com>
2026-05-09 13:20:35 +0700

    fixx5

--- app/test/selenium/tests/test_sel_login.py
+++ app/test/selenium/tests/test_sel_login.py
@@ -14,8 +14,11 @@ def test_login_success(driver):
     time.sleep(1)
 
     assert driver.current_url == 'http://127.0.0.1:5005/'
-    wait = WebDriverWait(driver, 5)
-    e = wait.until(lambda d: d.find_element(By.CSS_SELECTOR, '#mynavbar > div > div > a > span'))
+    wait = WebDriverWait(driver, 10)
+    locator = (By.CSS_SELECTOR, '#mynavbar > div > div > a > span')
+    wait.until(ec.presence_of_element_located(locator))
+    wait.until(ec.text_to_be_present_in_element(locator, 'user123'))
+    e = driver.find_element(*locator)
     assert 'user123' in e.text
 
 def test_login_failure(driver):
